2f8a4ab9411cc5b04506ee2d8677488670ea4b8c
patrykgalecki <patryk.galecki@student.wat.edu.pl>
2024-11-25 13:23:33 +0100

    chore: dataset update

---
+++ main.py
@@ -0,0 +1,13 @@
+users: list = [
+    {'name': 'Patrycja', 'posts': 1, 'city': 'Warszawa'},
+    {'name': 'Dominik', 'posts': 3, 'city': 'Warszawa'},
+    {'name': 'Szymon z wąsem', 'posts': 5, 'city': 'Warszawa'},
+    {'name': 'Szymon', 'posts': 7, 'city': 'Warszawa'},
+    {'name': 'Patryk', 'posts': 9, 'city': 'Warszawa'},
+
+]
+print(f'Witaj {users[0]['name']}!!')
+for user in users[1:]:
+    print(f'twój znajomy {user['name']},z {user['city']}, opublikował {user['posts']} postów')
+# for idx, _ in enumerate(users[1:]):
+#     print(f'twój znajomy {users[idx]}, opublikował {postow[idx]} postów')
