733036f23ebf415164e353c9973f2890484042fc
patrykgalecki <patryk.galecki@student.wat.edu.pl>
2024-12-02 14:33:41 +0100

    feat: add remove user function

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -15,3 +15,11 @@ def add_user(userlist:list)->None:
     new_city:str=input("podaj miasto ")
     new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
     userlist.append(new_user)
+
+
+def remove_user(userlist:list)->None:
+    user_to_find:str=input("podaj imię do usunięcia:")
+    for user in userlist:
+        if user['name'] == user_to_find:
+            print(f'usuwam: {user}')
+            userlist.remove(user)
--- mapbook/notatnik.py
+++ mapbook/notatnik.py
@@ -1,11 +1,20 @@
 users: list = [
     {'name': 'Patryk', 'posts': 1, 'city': 'Warszawa'},
-    {'name': 'Patrycja', 'posts': 1, 'city': 'Warszawa'},
+    {'name': 'Patryk', 'posts': 1, 'city': 'Warszawa'},
+    {'name': 'Tomek', 'posts': 1, 'city': 'Warszawa'},
+    {'name': 'Julia', 'posts': 1, 'city': 'Warszawa'},
 
 ]
 
-#
-add_user(users)
+def remove_user(userlist:list)->None:
+    user_to_find:str=input("podaj imię do usunięcia:")
+    for user in userlist:
+        if user['name'] == user_to_find:
+            print(f'usuwam: {user}')
+            userlist.remove(user)
+
+remove_user(users)
+
 for user in users:
     print(user)
 # print(new_user)
