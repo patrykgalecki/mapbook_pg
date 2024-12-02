16befee23bb06aa74c62cea8f8f396eed925e1eb
patrykgalecki <patryk.galecki@student.wat.edu.pl>
2024-12-02 12:58:09 +0100

    feat: add create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -7,3 +7,11 @@ def hello(user:str)->None:
 def read_users(users:list)->None:
     for user in users[1:]:
         print(f'twój znajomy {user['name']},z {user['city']}, opublikował {user['posts']} postów')
+
+
+def add_user(userlist:list)->None:
+    new_name:str=input("proszę podać imię nowego znajomego ")
+    new_posts:int=int(input("podaj liczbę postów "))
+    new_city:str=input("podaj miasto ")
+    new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
+    userlist.append(new_user)
---
+++ mapbook/notatnik.py
@@ -0,0 +1,11 @@
+users: list = [
+    {'name': 'Patryk', 'posts': 1, 'city': 'Warszawa'},
+    {'name': 'Patrycja', 'posts': 1, 'city': 'Warszawa'},
+
+]
+
+#
+add_user(users)
+for user in users:
+    print(user)
+# print(new_user)
