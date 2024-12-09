d2d18b5653273dd0be9ad26f02da4f9a241a621e
patrykgalecki <patryk.galecki@student.wat.edu.pl>
2024-12-09 12:21:31 +0100

    feat: menu add

--- mapbook/notatnik.py
+++ mapbook/notatnik.py
@@ -1,3 +1,5 @@
+from mapbook.crud import add_user, read_users
+
 users: list = [
     {'name': 'Patryk', 'posts': 1, 'city': 'Warszawa'},
     {'name': 'Patryk', 'posts': 1, 'city': 'Warszawa'},
@@ -27,23 +29,32 @@ def update_user(userlist:list)->None:
             user["city"] = new_city
 
 
-update_user(users)
+# update_user(users)
+
+while True:
+    print("======MENU======")
+    print("0. Wyjście z programu")
+    print("1. Dodaj znajomego")
+    print("2. Wczytaj znajomego")
+    print("3. Aktualizuj znajomego")
+    print("4. Usuń znajomego")
+    print("5. Generuj mapę z lokalizacją znajomego")
+    print("6. Generuj mapę z lokalizacją wszystkich znajomych")
 
 
-user_to_find:str=input("podaj imię użytkownika do aktualizacji:")
-for user in users:
-    if user['name'] == user_to_find:
-        new_name: str = input("proszę podać nowe imię znajomego ")
-        new_posts: int = int(input("podaj nową liczbę postów "))
-        new_city: str = input("podaj nowe miasto ")
-        user["name"]= new_name
-        user["posts"]= new_posts
-        user["city"] = new_city
-
-
-
-
-
-for user in users:
-    print(user)
-# print(new_user)
+    menu_option:str=input("użytkowniku wybierz opcje menu:")
+    print(f"użytkownik wybrał {menu_option}")
+    if menu_option == "0":
+        break
+    if menu_option == "1":
+        add_user(users)
+    if menu_option == "2":
+        read_users(users)
+    if menu_option == "3":
+        update_user(users)
+    if menu_option == "4":
+        remove_user(users)
+    if menu_option == "5":
+        pass
+    if menu_option == "6":
+        pass
